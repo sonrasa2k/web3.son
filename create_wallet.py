c96ae0d48a23044822fb32b0ba8ead9c6c5455a6
sonrasa2k <sonnguyen663615@gmail.com>
2022-05-02 17:24:24 +0700

    Get Price BTC

--- create_wallet.py
+++ create_wallet.py
@@ -9,13 +9,3 @@ print("Private Key: "+ private_key)
 #tạo vi tu private key
 wallet = connect.eth.account.privateKeyToAccount(private_key)
 print("Address :  " + wallet.address)
-
-
-function ax_b(float a,float b){
-    if(a==0){
-        return False;
-}
-    else{
-        return float(-b/a);
-}
-}
